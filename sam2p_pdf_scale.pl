979e54fd8d06c219a413b18790574893bbe6cf93
Peter Szabo <pts@fazekas.hu>
2009-06-14 11:36:34 +0000

    t

--- sam2p_pdf_scale.pl
+++ sam2p_pdf_scale.pl
@@ -13,11 +13,13 @@ eval '(exit $?0)' && eval 'PERL_BADLANG=x;PATH="$PATH:.";export PERL_BADLANG\
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
-#s
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
+#
+# TODO(pts): Rotate the image by 90 degrees if necessary.
 
 use integer;
 use strict;
