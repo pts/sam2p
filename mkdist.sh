8804d20808d482db043d7d6864af22b6084243ec
Peter Szabo <pts@fazekas.hu>
2010-04-25 10:25:43 +0000

    t

--- mkdist.sh
+++ mkdist.sh
@@ -2,17 +2,21 @@
 #
 # mkdist.sh by pts@fazekas.hu at Wed Mar  6 09:09:01 CET 2002
 # added debian/changelog support at Fri Mar  5 19:37:45 CET 2004
+# added libevhdns Makefile.in support at Sun Apr 25 12:24:43 CEST 2010
 # based on mkdist.sh of autotrace
 #
-#
 
-if [ -f debian/changelog ]; then :; else
-  echo "$0: missing: debian/changelog" >&2
-  exit 2
+if test "$1" = --cd; then
+  MYDIR="${0%/*}"
+  test "$MYDIR" || MYDIR=.
+  cd "$MYDIR"
 fi
-if [ -f files ]; then
+echo "Creating distfile in $PWD"
+
+# Get the list of files.
+if test -f files; then
   FILES="`cat files`"
-elif [ -d CVS ]; then
+elif test -d CVS; then
   FILES=$( IFS='
 '
     find -type d -name CVS | while read D; do
@@ -22,49 +26,63 @@ elif [ -d CVS ]; then
       perl -ne 'print"$ENV{E}$1\n"if m@^/([^/]+)/[1-9]@' <"$F"
     done)
 else
-  echo "$0: missing: files" >&2
+  echo "$0: missing: files or **/CVS/Entries" >&2
   exit 3
 fi
-PRO_VER="`<debian/changelog perl -ne 'print"$1-$2"if/^(\S+) +[(]([-.\w]+?)(?:-\d+)?[)] +\w+;/;last'`"
-if [ "$PRO_VER" ]; then :; else
-  echo "$0: couldn't determine version from debian/changelog" >&2
-  exit 4
+
+# Get the release version number.
+if test -f debian/changelog; then
+  # PRODUCT_AND_VERSION=$PRODUCT-$VERSION
+  PRODUCT_AND_VERSION="`<debian/changelog perl -ne 'print"$1-$2"if/^(\S+) +[(]([-.\w]+?)(?:-\d+)?[)] +\w+;/;last'`"
+  if test "$PRODUCT_AND_VERSION"; then :; else
+    echo "$0: couldn't determine version from debian/changelog" >&2
+    exit 4
+  fi
+elif test -f Makefile.in && grep '^product-and-version:' <Makefile.in >/dev/null; then
+  PRODUCT_AND_VERSION="`make SHELL=/bin/bash -f Makefile.in product-and-version`"
+  if test "$?" != 0 || test -z "$PRODUCT_AND_VERSION"; then
+    echo "$0: couldn't determine version from Makefile.in" >&2
+    exit 7
+  fi
+else
+  echo "$0: missing: debian/changelog or RELEASE= in Makefile.in" >&2
+  exit 2
 fi
-if [ -e "$PRO_VER" ]; then
-  echo "$0: $PRO_VER already exists, remove it first" >&2
+
+if test -e "$PRODUCT_AND_VERSION"; then
+  echo "$0: directory $PRODUCT_AND_VERSION already exists, remove it first" >&2
   exit 5
 fi
 
-if [ $# -gt 0 ]; then
+if test $# -gt 0; then
   TGZ_NAME="$1.tar.gz"; shift
 else
-  TGZ_NAME="$PRO_VER.tar.gz"
+  TGZ_NAME="$PRODUCT_AND_VERSION.tar.gz"
 fi
 
 set -e # exit on error
 rm -f "../$TGZ_NAME"
-mkdir "$PRO_VER"
-echo "$PRO_VER"
+mkdir "$PRODUCT_AND_VERSION"
 (IFS='
 '; exec tar -c -- $FILES "$@") |
-(cd "$PRO_VER" && exec tar -xv)
+(cd "$PRODUCT_AND_VERSION" && exec tar -xv)
 # ^^^ tar(1) magically calls mkdir(2) etc.
 
 # vvv Dat: don't include sam2p-.../ in the filenames of the .tar.gz
 #(IFS='
-#'; cd "$PRO_VER" && exec tar -czf "../../$TGZ_NAME" -- $FILES "$@")
+#'; cd "$PRODUCT_AND_VERSION" && exec tar -czf "../../$TGZ_NAME" -- $FILES "$@")
 
 # vvv Dat: do include sam2p-.../ in the filenames of the .tar.gz
 (IFS='
-'; export PRO_VER; exec tar -czf "../$TGZ_NAME" -- `echo "$FILES" | perl -pe '$_="$ENV{PRO_VER}/$_"'` "$@")
+'; export PRODUCT_AND_VERSION; exec tar -czf "../$TGZ_NAME" -- `echo "$FILES" | perl -pe '$_="$ENV{PRODUCT_AND_VERSION}/$_"'` "$@")
 
-rm -rf "$PRO_VER"
+rm -rf "$PRODUCT_AND_VERSION"
 set +e
 
-if [ -s "../$TGZ_NAME" ]; then :; else
-  echo "$0: failed to create dist: ../$TGZ_NAME" >&2
+if test -s "../$TGZ_NAME"; then :; else
+  echo "$0: failed to create distfile: ../$TGZ_NAME" >&2
   exit 6
 fi
-(cd ..; echo "Created dist: `pwd`/$TGZ_NAME" >&2)
+(cd ..; echo "Created distfile: `pwd`/$TGZ_NAME" >&2)
 
 # __EOF__
