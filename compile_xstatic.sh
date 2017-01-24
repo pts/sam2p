f8bd590324e9591d16ebea3e87fd4206dd8b875f
Peter Szabo <pts@fazekas.hu>
2017-01-24 19:40:36 +0100

    added compile_xstatic.sh; removed compile_uclibc.sh

--- compile_xstatic.sh
+++ compile_xstatic.sh
@@ -1,12 +1,7 @@
 #! /bin/bash --
-
-# Imp:
-# by pts@fazekas.hu at Fri May 23 23:33:19 CEST 2008
+# by pts@fazekas.hu at Tue Jan 24 19:35:49 CET 2017
 
 set -ex
-
-export  CC='/usr/src/uclibc-compiler/bin/i686-gcc -static'
-export CXX='/usr/src/uclibc-compiler/bin/i686-g++ -static'
 
 if test -f bts2.tth; then :; else
   ./configure --enable-lzw --enable-gif
@@ -17,11 +12,12 @@ SAM2P_VERSION="$(bash ./mkdist.sh --getversion)"
 test "$SAM2P_VERSION"
 
 # vvv either -mwindows or -mconsole
-$CXX -s -DNDEBUG -O3 \
+xstatic g++ -s -DNDEBUG -O3 \
     -DHAVE_CONFIG2_H -DUSE_CONFIG_UCLIBC_H -DSAM2P_VERSION=\""$SAM2P_VERSION"\" \
     -fsigned-char -fno-rtti -fno-exceptions -ansi -pedantic -Wall -W \
     sam2p_main.cpp appliers.cpp crc32.c c_lgcc.cpp in_ps.cpp in_tga.cpp in_pnm.cpp in_bmp.cpp in_gif.cpp in_lbm.cpp in_xpm.cpp mapping.cpp in_pcx.cpp in_jai.cpp in_png.cpp in_jpeg.cpp in_tiff.cpp rule.cpp minips.cpp encoder.cpp pts_lzw.c pts_fax.c pts_defl.c error.cpp image.cpp gensio.cpp snprintf.c gensi.cpp out_gif.cpp \
-    -o sam2p
-upx.pts --brute sam2p
-elfosfix.pl sam2p
-
+    -o sam2p.xstatic.uncompressed
+elfosfix.pl sam2p.xstatic.uncompressed
+cp -a sam2p.xstatic.uncompressed sam2p.xstatic
+upx.pts --brute sam2p.xstatic
+elfosfix.pl sam2p.xstatic
