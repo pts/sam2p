9b0f8b77b5fb0af42e8faf8fb63a0760e214757b
Peter Szabo <pts@fazekas.hu>
2024-09-17 10:15:13 +0200

    added -static-libgcc flag for i686-w64-mingw32-g++, fixing https://github.com/pts/sam2p/issues/78

--- compile_mingw.sh
+++ compile_mingw.sh
@@ -13,7 +13,7 @@ SAM2P_VERSION="$(set -- --getversion; . ./mkdist.sh)"
 test "$SAM2P_VERSION"
 
 # vvv either -mwindows or -mconsole
-i686-w64-mingw32-g++ -mconsole -s -DNDEBUG -O3 \
+i686-w64-mingw32-g++ -mconsole -static-libgcc -s -DNDEBUG -O3 \
     -DHAVE_CONFIG2_H -DUSE_CONFIG_MINGW_H -DSAM2P_VERSION=\""$SAM2P_VERSION"\" \
     -fsigned-char -fno-rtti -fno-exceptions -ansi -pedantic -Wall -W \
     sam2p_main.cpp appliers.cpp crc32.c c_lgcc.cpp in_ps.cpp in_tga.cpp in_pnm.cpp in_bmp.cpp in_gif.cpp in_lbm.cpp in_xpm.cpp mapping.cpp in_pcx.cpp in_jai.cpp in_png.cpp in_jpeg.cpp in_tiff.cpp rule.cpp minips.cpp encoder.cpp pts_lzw.c pts_fax.c pts_defl.c error.cpp image.cpp gensio.cpp snprintf.c gensi.cpp out_gif.cpp \
